eb9071b07122c57899035f4aa3e1e0d46639179b
nhanaldo <hothanhnha1207@gmail.com>
2023-11-28 08:23:30 +0700

    bai2

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <p className={styles.text}>123</p>        
+        <p className={styles.text}>1273</p>        
       </main>
     </>
   )
