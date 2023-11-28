288b8fc5f3c83db4634ad5da2a4733d1b6acccdf
nhanaldo <hothanhnha1207@gmail.com>
2023-11-28 09:37:20 +0700

    bai3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [value, setvalue] = useState({
+    username: "",
+    password: "",
+  });
+
+  const eventValueOnchang = (e) => {
+    setvalue({
+      ...value,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <>
       <Head>
@@ -15,7 +28,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <p className={styles.text}>1273</p>        
+        <div>Tai khoan</div>
+        <input name="username" onChange={eventValueOnchang} />
+        <div>Mat Khau</div>
+        <input name="password" onChange={eventValueOnchang} />
+
+        <div
+          onClick={() => {
+            console.log(value);
+          }}
+        >
+          nut
+        </div>
       </main>
     </>
   )
